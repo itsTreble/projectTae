56ddd140115058e83d39219b9c5e37faba414a3d
thoa huynh <chaitaes@gmail.com>
2018-01-26 23:12:49 -0700

    Added text interface

---
+++ Assets/TypingManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypingManager : MonoBehaviour {
+    public Text helperText;
+    string currentStringToType = "Mai is so cool";
+    // Use this for initialization
+    static TypingManager _instance;
+
+    public static TypingManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                //GameObject go = new GameObject("TypingManager");
+                //go.AddComponent<SoundManager>();
+            }
+            return _instance;
+        }
+    }
+    public bool CheckEqual(string input)
+    {
+        //if(inpu)
+        if(input.Equals(currentStringToType))
+        {
+            return true;
+        }
+        return false;
+    }
+    public void EnterString(string input)
+    {
+
+    }
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
